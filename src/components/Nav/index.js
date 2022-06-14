10e038f89da21914061a20ebaba2c2fd68671732
ravnishgupta <ravnish@gmail.com>
2022-06-13 18:07:46 -0700

    additional components

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,7 +2,17 @@ import React from "react";
 
 
 function Nav () {
-    return;
+    return (
+        <div className="container gap-3">
+            <div className="row">
+                <div className="col-sm-4">Ravnish Gupta</div>
+                <div className="col-sm-2">About Me</div>
+                <div className="col-sm-2">Portfolio</div>
+                <div className="col-sm-2">Contact</div>
+                <div className="col-sm-2">Resume</div>
+            </div>
+        </div>
+    );
 }
 
 export default Nav;
