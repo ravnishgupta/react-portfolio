37a951625b0db96b4ea80e85d57f619f4631cb1a
ravnishgupta <ravnish@gmail.com>
2022-06-15 17:25:43 -0700

    contact form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
+import Footer from './components/Footer';
+import ContactForm from './components/Contact';
+
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'about me',
+      description: 'About Me',
+    },
+    { name: 'portfolio', description: 'Portraits of people in my life' },
+    { name: 'contact', description: 'Delicious delicacies' },
+    { name: 'resume', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]); 
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
   return (
     <div>
-      <Nav />
+      <Nav 
+      categories={categories}
+      setCurrentCategory={setCurrentCategory}
+      currentCategory={currentCategory}
+    />
+      <ContactForm />
+      <Footer />
     </div>
+
   );
 }
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer () {
+    return (
+      <footer>
+        <div className = "row">
+            <div className="col-6 text-center">LinkedIn</div>
+            <div className="col-6 text-center">Github</div>
+        </div>
+      </footer>
+    )
+}
+
+export default Footer;
