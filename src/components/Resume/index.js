85efd159b92363ae021b9a4e003c2637bf976619
ravnishgupta <ravnish@gmail.com>
2022-06-20 21:57:52 -0700

    added reesume link

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Resume() {
     return (
         <div>
-            My Resume
+            My resume can be accessed <a href='https://drive.google.com/file/d/1uZ7JF9GGNF5nxRmtqMnbeoesFbZX-H9u/view?usp=sharing'>here</a>. 
         </div>
     )
 }
